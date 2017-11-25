47e39b8a9d3e75152ce53b1e12d1eff81188d3e6
mysterybiscuit <guest91111@yandex.com>
2017-11-25 11:21:56 +0100

    Adding time feature
    In the process of adding a feature to keep track of time.

---
+++ 30 Seconds History/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int timeLimit = 30;
+
+    public float timeLeft;
+
+    public Image img;
+
+	// Use this for initialization
+	void Start () {
+        timeLeft = timeLimit;
+	}
+	
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        img.color = Color.Lerp(Color.green, Color.red, 0.5f);
+	}
+}
