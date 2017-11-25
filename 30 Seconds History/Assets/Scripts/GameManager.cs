7bb51fc6c6354ac42c83861992bf131aa7f642aa
mysterybiscuit <guest91111@yandex.com>
2017-11-25 12:17:15 +0100

    Timer works
    I have made it so that the timer now works.

--- 30 Seconds History/Assets/Scripts/GameManager.cs
+++ 30 Seconds History/Assets/Scripts/GameManager.cs
@@ -7,22 +7,43 @@ public class GameManager : MonoBehaviour {
 
     public int timeLimit = 30;
 
+    public float timePassed;
+
     public float timeLeft;
+
+    public float linearInterp = 0;
+
+    public GameObject getRectTrans;
+
+    public Rect rectTrans;
 
     public Image img;
 
+    public Text timeText;
+
+    public Color beginColor;
+
+    private float widthConstant;
+
 	// Use this for initialization
 	void Start () {
+        timePassed = 0;
         timeLeft = timeLimit;
+        //rectTrans = getRectTrans.GetComponentInChildren<Rect>();
+        //widthConstant = rectTrans.xMax - rectTrans.xMin / (30 * 50);
 	}
 	
     void Update()
     {
+        timePassed += Time.deltaTime;
         timeLeft -= Time.deltaTime;
     }
 
 	// Update is called once per frame
 	void FixedUpdate () {
-        img.color = Color.Lerp(Color.green, Color.red, 0.5f);
+        linearInterp = timePassed / timeLimit;
+        //rectTrans.xMin += widthConstant;
+        img.color = Color.Lerp(beginColor, Color.red, linearInterp);
+        timeText.text = timeLeft.ToString("0");
 	}
 }
