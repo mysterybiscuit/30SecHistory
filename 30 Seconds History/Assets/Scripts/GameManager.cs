22fa5da9516443cdf2ee5987198369cf02d96b83
mysterybiscuit <guest91111@yandex.com>
2017-11-26 11:49:54 +0100

    Alpha 2.5
    Made the question generator work.
    TODO:
    *Generate new questions when the time has ended.

--- 30 Seconds History/Assets/Scripts/GameManager.cs
+++ 30 Seconds History/Assets/Scripts/GameManager.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public int timeLimit = 30;
+    public int timeLimit = 33;
 
     public float timePassed;
 
@@ -21,16 +22,54 @@ public class GameManager : MonoBehaviour {
 
     public Text timeText;
 
+    public Text[] textFields;
+
+    public Toggle[] checkBoxes;
+
+    public bool[] checkBoxesChecked = new bool[5];
+
     public Color beginColor;
 
-    private float widthConstant;
+    private Random rngesus = new Random();
 
-	// Use this for initialization
-	void Start () {
-        timePassed = 0;
-        timeLeft = timeLimit;
-        //rectTrans = getRectTrans.GetComponentInChildren<Rect>();
-        //widthConstant = rectTrans.xMax - rectTrans.xMin / (30 * 50);
+    private bool termsChosen = false;
+
+    public Term[] terms1;
+    public Term[] terms2;
+    public Term[] terms3;
+    public Term[] terms4;
+    public Term[] terms5;
+    public Term[] terms6;
+    public Term[] terms7;
+    public Term[] terms8;
+    public Term[] terms9;
+    public Term[] terms10;
+
+    private static List<Term> unused = new List<Term>();
+    private static List<int> chosenTimes = new List<int>();
+
+    private Term[] currentTerms = new Term[5];
+
+    // Use this for initialization
+    void Start () {
+        if (unused == null || unused.Count == 0)
+        {
+            unused = terms1.ToList<Term>();
+            //if (chosenTimes.Contains(1))
+            //{
+            //    foreach (Term term in terms1)
+            //    {
+            //        unused.Add(term);
+            //    }
+            //}
+            //if (chosenTimes.Contains(2))
+            //{
+            //    foreach (Term term in terms2)
+            //    {
+            //        unused.Add(term);
+            //    }
+            //}
+        }
 	}
 	
     void Update()
@@ -41,9 +80,87 @@ public class GameManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
+        if (!termsChosen && timeLeft < 30)
+        {
+            getRandomTerms();
+            termsChosen = true;
+        }
+        else if (timeLeft < 0)
+        {
+            timeLeft = timeLimit;
+            timePassed = 0;
+            getRandomTerms();
+        }
         linearInterp = timePassed / timeLimit;
         //rectTrans.xMin += widthConstant;
         img.color = Color.Lerp(beginColor, Color.red, linearInterp);
         timeText.text = timeLeft.ToString("0");
+        checkCheckBoxes();
 	}
+
+    public void onStartButtonPressed()
+    {
+        timePassed = 0;
+        timeLeft = timeLimit;
+    }
+
+    public void checkCheckBoxes()
+    {
+        if (checkBoxes[0].isOn)
+        {
+            checkBoxesChecked[0] = true;
+        }
+        else
+        {
+            checkBoxesChecked[0] = false;
+        }
+
+        if (checkBoxes[1].isOn)
+        {
+            checkBoxesChecked[1] = true;
+        }
+        else
+        {
+            checkBoxesChecked[1] = false;
+        }
+
+        if (checkBoxes[2].isOn)
+        {
+            checkBoxesChecked[2] = true;
+        }
+        else
+        {
+            checkBoxesChecked[2] = false;
+        }
+
+        if (checkBoxes[3].isOn)
+        {
+            checkBoxesChecked[3] = true;
+        }
+        else
+        {
+            checkBoxesChecked[3] = false;
+        }
+
+        if (checkBoxes[4].isOn)
+        {
+            checkBoxesChecked[4] = true;
+        }
+        else
+        {
+            checkBoxesChecked[4] = false;
+        }
+    }
+
+    void getRandomTerms()
+    {
+        for (int i = 0; i < currentTerms.Length; i++)
+        {
+            int randomInt = Random.Range(0, unused.Count);
+            textFields[i].text = unused[randomInt].term.ToString(); //Sets a new question from the list of unused questions
+            unused.RemoveAt(randomInt);
+        }
+        
+    }
+
 }
